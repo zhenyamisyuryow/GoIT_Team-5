146cf504d1dc98f8d2e9fb382cef7b33d2e35e82
Eugene M <eugene.misyuryow@gmail.com>
2023-08-04 13:06:44 +0300

    added base classes

---
+++ base_cls.py
@@ -0,0 +1,34 @@
+from collections import UserDict
+import re
+from datetime import datetime
+import pickle
+
+class Field:
+    pass
+
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
+
+
+class Email(Field):
+    pass
+
+
+class Birthday(Field):
+    pass
+
+
+class Address(Field):
+    pass
+
+
+class Note(UserDict):
+    pass
+
+
+class Contacts(UserDict):
+    pass
