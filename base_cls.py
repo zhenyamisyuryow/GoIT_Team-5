38ac035f4eabf0ed554b813187cd07e9486ec871
ViktorTil <tilnyakviktor@gmail.com>
2023-08-04 21:52:01 +0300

    phone_validation+ custom Except class-InputError

--- base_cls.py
+++ base_cls.py
@@ -3,6 +3,8 @@ import re
 from datetime import datetime
 import pickle
 
+class InputError(Exception):
+    pass
 class Field:
     pass
 
@@ -11,7 +13,27 @@ class Name(Field):
     pass
 
 class Phone(Field):
-    pass
+    def __init__(self, phone):
+        self.__value = None
+        self.value = phone
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, phone):
+        try:
+            pattern = r"\+?3?8?[- (]?0\d{2}[- )]?\d{3}[- ]?\d{2}[- ]?\d{2}$"
+            add_cod = "+380"
+            result = (re.search(pattern, phone)).group()
+            res_phone = re.sub(r"(\D)", "", result)
+            form_phone = add_cod[0:13-len(res_phone)] + res_phone
+            self.__value = form_phone
+
+        except AttributeError:
+            raise InputError(
+                f"Вводите корректно номера телефонов, например, в формате: \033[34m0XX-XXX-XX-XX\033[0m")
 
 
 class Email(Field):
