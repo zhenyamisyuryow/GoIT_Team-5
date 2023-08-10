513d954a4cd6a8d7885709f2a026fee263a3cd0d
Eugene M <eugene.misyuryow@gmail.com>
2023-08-10 15:31:09 +0300

    changed package name

--- virtual_assistant/setup.py
+++ virtual_assistant/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your_package_name',
+    name='virtual_assistant_py_wizards',
     version='1.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'virtual_assistant=virtual_assistant.main:main',  # Replace with your entry point
+            'virtual_assistant=virtual_assistant.main:main',
         ],
     },
 )
